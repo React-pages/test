3d508781ce0e1fcf0eec4ed1819fb753c59f656e
Akari Nishii <akarinishii@Akaris-MacBook-Pro.local>
2023-02-25 13:34:28 +0100

    test

--- src/App.js
+++ src/App.js
@@ -24,63 +24,21 @@ const BookingStopContainer = ({ middleSteps })=> {
   return (
     <Stack
             onMouseOver={(e) => {
-              // e.nativeEvent.stopPropagation()
-              // e.nativeEvent.stopImmediatePropagation()
-              // e.nativeEvent.preventDefault()
               if (!isMobile) {
                 setShowMiddleSteps(true)
               }
             }}
-            onMouseOut={(e) =>{
-              // e.nativeEvent.stopPropagation()
-              // e.nativeEvent.stopImmediatePropagation()
-              // e.nativeEvent.preventDefault()
+            onMouseOut={() =>{
               if (!isMobile) {
               setShowMiddleSteps(false)
             }
             }}
-            // onTouchMove={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onMouseDown={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onMouseEnter={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onMouseMove={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onMouseUp={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onTouchEnd={(e) => {
-            //   e.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.preventDefault()
-            // }}
-            // onTouchStart={(e) => {
-            //   e.nativeEvent.stopPropagation()
-            //   e.nativeEvent.stopImmediatePropagation()
-            //   e.nativeEvent.preventDefault()
-            //   setShowMiddleSteps(!showMiddleSteps)
-            // }}
             onClick={(e) => {
               e.stopPropagation()
               e.preventDefault()
-              e.nativeEvent.stopPropagation()
-              e.nativeEvent.stopImmediatePropagation()
-              e.nativeEvent.preventDefault()
+              // e.nativeEvent.stopPropagation()
+              // e.nativeEvent.stopImmediatePropagation()
+              // e.nativeEvent.preventDefault()
               if (isMobile) {
                 setShowMiddleSteps(!showMiddleSteps)
               }
